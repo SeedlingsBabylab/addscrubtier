ffa24cc0d4b07e2fa0a688e4f9c0471e6575e88e
Andrei Amatuni <andrei.amatuni@gmail.com>
2016-02-23 10:38:13 -0500

    Add subject/visit specifier arguments

--- README.md
+++ README.md
@@ -6,3 +6,27 @@
 ```bash
 $: python addscrub.py [unscrubbed_file.cha] [new_output_path.cha]
 ```
+
+## batchscrub
+
+```bash
+$: python batchscrub.py [start_dir] [subject] [visit]
+```
+
+e.g:
+
+```bash
+$: python batchscrub.py /Volumes/seedlings/Subject_Files all 14
+```
+
+```bash
+$: python batchscrub.py /Volumes/seedlings/Subject_Files all all
+```
+
+```bash
+$: python batchscrub.py /Volumes/seedlings/Subject_Files 02 14
+```
+
+```bash
+$: python batchscrub.py /Volumes/seedlings/Subject_Files 05 all
+```
--- batchscrub.py
+++ batchscrub.py
@@ -5,7 +5,10 @@ import subprocess as sp
 start_dir = ""
 subject = ""
 visit = ""
-scrubbed_root = "/Users/andrei/code/work/babylab/addscrubtier/scrubbed"
+
+#scrubbed_root = "/Users/andrei/code/work/babylab/addscrubtier/scrubbed"
+scrubbed_root = "/Users/andrei/code/work/babylab/addscrubtier/scrubbed12"
+
 
 def walk_tree():
     files_processed_count = 0
@@ -15,30 +18,37 @@ def walk_tree():
             for file in files:
                 print file
                 if "newclan_merged.cha" in file or "_final.cha" in file:
+                    if check_subject_visit(file):
+                        command = base + [os.path.join(root, file),
+                                          os.path.join(scrubbed_root, file.replace(".cha", "_scrubbed.cha"))]
 
-                   # if file[0:3] == subject and not visit:
+                        abbrev_command = [os.path.split(element)[1] for element in command]
+                        print "command: {}".format(abbrev_command)
+                        pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10**8)
+                        pipe.communicate()  # blocks until the subprocess in complete
+                        processed.write(file[0:5]+":\tcommand: {}\n".format(abbrev_command))
+                        files_processed_count += 1
 
-                    command = base + [os.path.join(root, file),
-                                      os.path.join(scrubbed_root, file.replace(".cha", "_scrubbed.cha"))]
-
-                    abbrev_command = [os.path.split(element)[1] for element in command]
-                    print "command: {}".format(abbrev_command)
-                    pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10**8)
-                    pipe.communicate()  # blocks until the subprocess in complete
-                    processed.write(file[0:5]+":\tcommand: {}\n".format(abbrev_command))
-                    files_processed_count += 1
         print "{} files processed".format(files_processed_count)
+
+
+def check_subject_visit(file):
+
+    if subject == "all" and visit == "all":
+        return True
+    if subject == "all" and visit == file[3:5]:
+        return True
+    if subject == file[0:2] and visit == "all":
+        return True
+    if subject == file[0:2] and visit == file[3:5]:
+        return True
+
+    return False
 
 if __name__ == "__main__":
 
     start_dir = sys.argv[1]
     subject = sys.argv[2]
     visit = sys.argv[3]
-
-    if subject == "all":
-        subject = ""
-
-    if visit == "all":
-        visit = ""
 
     walk_tree()
